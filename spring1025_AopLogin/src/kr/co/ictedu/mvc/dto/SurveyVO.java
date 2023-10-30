a440b2a2c8fe42d2221dad7125a4fe7589428e99
Cyeon <yeoneeso@naver.com>
2023-10-30 18:25:45 +0900

    -- 1030 lundi commit

---
+++ spring1025_AopLogin/src/kr/co/ictedu/mvc/dto/SurveyVO.java
@@ -0,0 +1,51 @@
+package kr.co.ictedu.mvc.dto;
+
+import java.util.List;
+
+public class SurveyVO {
+	private int num ;
+	private String sub ;
+	private int code ;
+	private String sdate ;
+	private int surveytotal ;
+	// 1:n °ü°è
+	private List<SurveyContentVO> subvey ;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getSub() {
+		return sub;
+	}
+	public void setSub(String sub) {
+		this.sub = sub;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getSdate() {
+		return sdate;
+	}
+	public void setSdate(String sdate) {
+		this.sdate = sdate;
+	}
+	public int getSurveytotal() {
+		return surveytotal;
+	}
+	public void setSurveytotal(int surveytotal) {
+		this.surveytotal = surveytotal;
+	}
+	public List<SurveyContentVO> getSubvey() {
+		return subvey;
+	}
+	public void setSubvey(List<SurveyContentVO> subvey) {
+		this.subvey = subvey;
+	}
+	
+}
