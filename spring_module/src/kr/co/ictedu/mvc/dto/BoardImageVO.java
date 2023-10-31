eb3d1f6a98ccd209a0c5d33fe16da1edeb5df6e5
Cyeon <yeoneeso@naver.com>
2023-10-31 18:19:39 +0900

    -- 1031 mardi commit

---
+++ spring_module/src/kr/co/ictedu/mvc/dto/BoardImageVO.java
@@ -0,0 +1,41 @@
+package kr.co.ictedu.mvc.dto;
+
+public class BoardImageVO {
+	   private int inum ;
+	   private String iname ;
+	   private String reip ;
+	   private String iregdate ;
+	   private int isThumb ;
+
+	   public int getInum() {
+	      return inum;
+	   }
+	   public void setInum(int inum) {
+	      this.inum = inum;
+	   }
+	   public String getIname() {
+	      return iname;
+	   }
+	   public void setIname(String iname) {
+	      this.iname = iname;
+	   }
+	   public String getReip() {
+	      return reip;
+	   }
+	   public void setReip(String reip) {
+	      this.reip = reip;
+	   }
+	   public String getIregdate() {
+	      return iregdate;
+	   }
+	   public void setIregdate(String iregdate) {
+	      this.iregdate = iregdate;
+	   }
+	   public int getIsThumb() {
+	      return isThumb;
+	   }
+	   public void setIsThumb(int isThumb) {
+	      this.isThumb = isThumb;
+	   }
+
+}
